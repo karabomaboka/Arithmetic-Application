472844d71fcc408a651e46c0cc49cf0872a01c9a
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-03-10 19:02:08 +0200

    Add files via upload

---
+++ frmArithmetic.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmArithmetic
+
+  Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+    Dim a As Double
+    Dim b As Double
+    a = 2
+    b = 3
+    lstResults.Items.Clear()
+    lstResults.Items.Add(Math.Sqrt(5 * b + 1))
+    lstResults.Items.Add(Int(a ^ b + 0.8))
+    lstResults.Items.Add(Math.Round(a / b, 3))
+  End Sub
+
+End Class
